1a51199c8c6ef08e23ffc8a62e49dfc9ba361a9e
Vincent van der Laan <vincent.van.der.laan@avivasolutions.nl>
2020-12-06 12:14:35 +0100

    Completed day 6

---
+++ Aoc2020.Tests/Day06Tests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Aoc2020.Tests
+{
+    public class Day06Tests
+    {
+        private readonly IDay _day;
+        private readonly List<string> _input;
+
+        public Day06Tests()
+        {
+            _input = new List<string>
+            {
+                "abc",
+                "",
+                "a",
+                "b",
+                "c",
+                "",
+                "ab",
+                "ac",
+                "",
+                "a",
+                "a",
+                "a",
+                "a",
+                "",
+                "b"
+            };
+
+            _day = new Day06();
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            _day.Solve1(_input).Should().Be(11);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            _day.Solve2(_input).Should().Be(6);
+        }
+    }
+}
--- Aoc2020/Day06.cs
+++ Aoc2020/Day06.cs
@@ -1,22 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Aoc2020
 {
     public class Day06 : IDay
     {
-        public bool IsImplemented => false;
-        public bool IsPart1Complete => false;
+        public bool IsImplemented => true;
+        public bool IsPart1Complete => true;
         public bool IsPart2Complete => false;
 
         public int Solve1(IList<string> input)
         {
-            throw new NotImplementedException();
+            var groupAnswers = ParseGroups(input);
+
+            return groupAnswers.Sum(x => x.AnyoneAnswered);
         }
 
         public int Solve2(IList<string> input)
         {
-            throw new NotImplementedException();
+            var groupAnswers = ParseGroups(input);
+
+            return groupAnswers.Sum(x => x.EveryoneAnswered());
+        }
+
+        private static List<Group> ParseGroups(IList<string> input)
+        {
+            var group = new Group();
+            var groupAnswers = new List<Group> { @group };
+            foreach (var answers in input)
+            {
+                if (answers.Length == 0)
+                {
+                    group = new Group();
+                    groupAnswers.Add(group);
+                }
+                else
+                {
+                    group.PersonAnswers.Add(answers);
+                }
+            }
+
+            return groupAnswers;
+        }
+    }
+
+    public class Group
+    {
+        public List<string> PersonAnswers { get; set; } = new List<string>();
+
+        public int AnyoneAnswered => string.Join("", PersonAnswers).Distinct().Count();
+
+        public int EveryoneAnswered()
+        {
+            var counter = 0;
+            for (var i = 0; i < 26; i++)
+            {
+                var toCheck = (char) (i + 97);
+                if (PersonAnswers.All(x => x.Contains(toCheck)))
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
         }
     }
 }
