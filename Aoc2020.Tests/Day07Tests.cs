8495f3cb19d0db5a925f1e98b0b4a6b811815b48
Vincent van der Laan <vincent.van.der.laan@avivasolutions.nl>
2020-12-07 09:56:22 +0100

    Day 7 - Part 1

---
+++ Aoc2020.Tests/Day07Tests.cs
@@ -0,0 +1,69 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Aoc2020.Tests
+{
+    public class Day07Tests
+    {
+        private readonly IDay _day;
+        private List<string> _input;
+
+        public Day07Tests()
+        {
+            _day = new Day07();
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            _input = new List<string>
+            {
+                "light red bags contain 1 bright white bag, 2 muted yellow bags.",
+                "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+                "bright white bags contain 1 shiny gold bag.",
+                "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+                "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+                "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+                "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+                "faded blue bags contain no other bags.",
+                "dotted black bags contain no other bags.",
+            };
+            _day.Solve1(_input).Should().Be(4);
+        }
+
+        [Fact]
+        public void Part2_option1()
+        {
+            _input = new List<string>
+            {
+                "light red bags contain 1 bright white bag, 2 muted yellow bags.",
+                "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+                "bright white bags contain 1 shiny gold bag.",
+                "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+                "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+                "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+                "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+                "faded blue bags contain no other bags.",
+                "dotted black bags contain no other bags.",
+            };
+            _day.Solve2(_input).Should().Be(32);
+        }
+
+        [Fact]
+        public void Part2_option2()
+        {
+            _input = new List<string>
+            {
+                "shiny gold bags contain 2 dark red bags.",
+                "dark red bags contain 2 dark orange bags.",
+                "dark orange bags contain 2 dark yellow bags.",
+                "dark yellow bags contain 2 dark green bags.",
+                "dark green bags contain 2 dark blue bags.",
+                "dark blue bags contain 2 dark violet bags.",
+                "dark violet bags contain no other bags.",
+            };
+            _day.Solve2(_input).Should().Be(126);
+        }
+    }
+}
--- Aoc2020/Day07.cs
+++ Aoc2020/Day07.cs
@@ -1,22 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Aoc2020
 {
     public class Day07 : IDay
     {
-        public bool IsImplemented => false;
-        public bool IsPart1Complete => false;
+        public bool IsImplemented => true;
+        public bool IsPart1Complete => true;
         public bool IsPart2Complete => false;
+
+        private Dictionary<string, List<ContainingBag>> bagCombinations;
 
         public int Solve1(IList<string> input)
         {
-            throw new NotImplementedException();
+            bagCombinations = RenderBags(input);
+            return FindColorPossibilities("shiny gold", new List<string>()).Distinct().Count();
         }
 
         public int Solve2(IList<string> input)
         {
-            throw new NotImplementedException();
+            bagCombinations = RenderBags(input);
+            return 0;
         }
+
+        private Dictionary<string, List<ContainingBag>> RenderBags(IList<string> input)
+        {
+            var bags = new Dictionary<string, List<ContainingBag>>();
+
+            foreach (var bagRow in input)
+            {
+                var colorPosition = bagRow.IndexOf("bags");
+                var color = bagRow.Substring(0, colorPosition).Trim();
+                
+                var containingString = bagRow.Substring(colorPosition + 12).Trim();
+                var containingArray = containingString.Split(',');
+                var containingList = new List<ContainingBag>();
+                foreach (var containingInput in containingArray)
+                {
+                    var containingBag = containingInput.Trim();
+                    if (containingBag.StartsWith("no other bags"))
+                    {
+                        continue;
+                    }
+
+                    var amountPosition = containingBag.IndexOf(' ');
+                    var containingAmount = int.Parse(containingBag.Substring(0, amountPosition));
+                    var containingColor = containingBag.Substring(amountPosition+1, containingBag.LastIndexOf(' ') - amountPosition).Trim();
+                    
+                    containingList.Add(new ContainingBag { Amount = containingAmount, Color = containingColor});
+                }
+
+                bags.Add(color, containingList);
+            }
+
+            return bags;
+        }
+
+        private List<string> FindColorPossibilities(string color, List<string> colorPossibilities)
+        {
+            var possibilities = bagCombinations
+                .Where(x => x.Value.Any(y => y.Color == color))
+                .ToList();
+            colorPossibilities.AddRange(possibilities.Select(x => x.Key));
+
+            foreach (var possibility in possibilities)
+            {
+                FindColorPossibilities(possibility.Key, colorPossibilities);
+            }
+
+            return colorPossibilities;
+        }
+    }
+
+    public class ContainingBag
+    {
+        public int Amount { get; set; }
+
+        public string Color { get; set; }
     }
 }
