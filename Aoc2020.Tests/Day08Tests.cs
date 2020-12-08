a96b82da706e27b8974da1e0ea3c13e78e5ea587
Vincent van der Laan <vincent.van.der.laan@avivasolutions.nl>
2020-12-08 09:08:00 +0100

    Day 8

---
+++ Aoc2020.Tests/Day08Tests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Aoc2020.Tests
+{
+    public class Day08Tests
+    {
+        private readonly IDay _day;
+        private readonly List<string> _input;
+
+        public Day08Tests()
+        {
+            _input = new List<string>
+            {
+                "nop +0",
+                "acc +1",
+                "jmp +4",
+                "acc +3",
+                "jmp -3",
+                "acc -99",
+                "acc +1",
+                "jmp -4",
+                "acc +6",
+            };
+
+            _day = new Day08();
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            _day.Solve1(_input).Should().Be(5);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            _day.Solve2(_input).Should().Be(8);
+        }
+    }
+}
--- Aoc2020/Day08.cs
+++ Aoc2020/Day08.cs
@@ -1,22 +1,105 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Aoc2020
 {
     public class Day08 : IDay
     {
-        public bool IsImplemented => false;
-        public bool IsPart1Complete => false;
+        public bool IsImplemented => true;
+        public bool IsPart1Complete => true;
         public bool IsPart2Complete => false;
 
         public int Solve1(IList<string> input)
         {
-            throw new NotImplementedException();
+            var commands = input.Select(x => HandheldGameConsole.ParseCommand(x)).ToList();
+
+            var handheld = new HandheldGameConsole(commands);
+            handheld.Boot();
+
+            return handheld.Accumulator;
         }
 
         public int Solve2(IList<string> input)
         {
-            throw new NotImplementedException();
+            var commands = input.Select(x => HandheldGameConsole.ParseCommand(x)).ToList();
+
+            for (var i = 0; i < commands.Count; i++)
+            {
+                if (commands[i].instruction == "acc") continue;
+
+                var copy = commands.ToList();
+                copy[i] = (SwitchOperation(commands[i].instruction), commands[i].number);
+
+                var handheld = new HandheldGameConsole(copy);
+                var completed = handheld.Boot();
+
+                if (completed)
+                {
+                    return handheld.Accumulator;
+                }
+            }
+
+            return 0;
+        }
+
+        private string SwitchOperation(string operation)
+        {
+            return operation switch
+            {
+                "jmp" => "nop",
+                "nop" => "jmp",
+                _ => throw new Exception("Incorrect operation switch")
+            };
+        }
+    }
+
+    public class HandheldGameConsole
+    {
+        private readonly IList<(string instruction, int number)> _commands;
+
+        public int Accumulator { get; private set; }
+
+        public HandheldGameConsole(IList<(string instruction, int number)> commands)
+        {
+            _commands = commands;
+            Accumulator = 0;
+        }
+
+        public bool Boot()
+        {
+            var rowPosition = 0;
+            var executedRows = new List<int>();
+
+            while (executedRows.All(x => x != rowPosition) && rowPosition < _commands.Count)
+            {
+                executedRows.Add(rowPosition);
+
+                var (instruction, number) = _commands[rowPosition];
+                switch (instruction)
+                {
+                    case "nop":
+                        rowPosition++;
+                        break;
+
+                    case "acc":
+                        Accumulator += number;
+                        rowPosition++;
+                        break;
+
+                    case "jmp":
+                        rowPosition += number;
+                        break;
+                }
+            }
+
+            return rowPosition >= _commands.Count;
+        }
+
+        public static (string instruction, int number) ParseCommand(string line)
+        {
+            var commandParts = line.Split(' ');
+            return (commandParts[0], int.Parse(commandParts[1]));
         }
     }
 }
