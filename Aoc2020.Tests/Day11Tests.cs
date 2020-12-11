c20ecce42b456fa454ef4363e4b98df146ea2463
Vincent van der Laan <vincent.van.der.laan@avivasolutions.nl>
2020-12-11 10:26:01 +0100

    Day 11 - Part 1 & partailly part 2

---
+++ Aoc2020.Tests/Day11Tests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Aoc2020.Tests
+{
+    public class Day11Tests
+    {
+        private readonly IDay _day;
+        private List<string> _input;
+
+        public Day11Tests()
+        {
+            _input = new List<string>
+            {
+                "L.LL.LL.LL",
+                "LLLLLLL.LL",
+                "L.L.L..L..",
+                "LLLL.LL.LL",
+                "L.LL.LL.LL",
+                "L.LLLLL.LL",
+                "..L.L.....",
+                "LLLLLLLLLL",
+                "L.LLLLLL.L",
+                "L.LLLLL.LL",
+            };
+
+            _day = new Day11();
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            _day.Solve1(_input).Should().Be(37);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            _day.Solve2(_input).Should().Be(26);
+        }
+    }
+}
--- Aoc2020/Day11.cs
+++ Aoc2020/Day11.cs
@@ -1,22 +1,135 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Aoc2020
 {
     public class Day11 : IDay
     {
-        public bool IsImplemented => false;
-        public bool IsPart1Complete => false;
+        public bool IsImplemented => true;
+        public bool IsPart1Complete => true;
         public bool IsPart2Complete => false;
+
+        private List<string> WaitingArea { get; set; }
+
+        private int OccupiedWaitingRoom => WaitingArea.Sum(x => x.Count(y => y == OccupiedSeat));
+
+        private int WaitingRoomMaxX = 0;
+        private int WaitingRoomMaxY = 0;
+
+        private const char Floor = '.';
+        private const char EmptySeat = 'L';
+        private const char OccupiedSeat = '#';
 
         public long Solve1(IList<string> input)
         {
-            throw new NotImplementedException();
+            return Solve(input.ToList(), CountOccupiedSeatsAround, 4);
         }
 
         public long Solve2(IList<string> input)
         {
-            throw new NotImplementedException();
+            return Solve(input.ToList(), CountOccupiedSeatsAdvanced, 5);
+        }
+
+        private long Solve(List<string> input, Func<int, int, int> CountSeatsAround, int clearAround)
+        {
+            WaitingArea = input.ToList();
+            WaitingRoomMaxX = WaitingArea.First().Length - 1;
+            WaitingRoomMaxY = WaitingArea.Count - 1;
+
+            var occupied = -1;
+
+            while (occupied != OccupiedWaitingRoom)
+            {
+                occupied = OccupiedWaitingRoom;
+
+                var copy = WaitingArea.Select(x => x.ToCharArray()).ToList();
+                for (var y = 0;y <= WaitingRoomMaxY;y++)
+                {
+                    for (var x = 0;x <= WaitingRoomMaxX;x++)
+                    {
+                        if (copy[y][x] == Floor)
+                        {
+                            continue;
+                        }
+
+                        var occupiedAround = CountSeatsAround(y, x);
+                        if (copy[y][x] == EmptySeat && occupiedAround == 0)
+                        {
+                            copy[y][x] = OccupiedSeat;
+                        }
+                        if (copy[y][x] == OccupiedSeat && occupiedAround >= clearAround)
+                        {
+                            copy[y][x] = EmptySeat;
+                        }
+                    }
+                }
+
+                WaitingArea = copy.Select(x => string.Join("", x)).ToList();
+            }
+
+            return OccupiedWaitingRoom;
+        }
+
+        private int CountOccupiedSeatsAround(int y, int x)
+        {
+            var tilesAround = new List<char>
+            {
+                GetTile(y-1, x-1),
+                GetTile(y-1, x),
+                GetTile(y-1, x+1),
+
+                GetTile(y, x-1),
+                GetTile(y, x+1),
+
+                GetTile(y+1, x-1),
+                GetTile(y+1, x),
+                GetTile(y+1, x+1),
+            };
+
+            return tilesAround.Count(z => z == OccupiedSeat);
+        }
+
+        private char GetTile(int y, int x)
+        {
+            if (y < 0 || y > WaitingRoomMaxY || x < 0 || x > WaitingRoomMaxY)
+            {
+                return Floor;
+            }
+
+            return WaitingArea[y][x];
+        }
+
+        private int CountOccupiedSeatsAdvanced(int y, int x)
+        {
+            var tilesAround = new List<char>
+            {
+                GetFirstSeat(y-1, x-1, -1, -1),
+                GetFirstSeat(y-1, x, -1, 0),
+                GetFirstSeat(y-1, x+1, -1, +1),
+
+                GetFirstSeat(y, x-1, 0, -1),
+                GetFirstSeat(y, x+1, 0, +1),
+
+                GetFirstSeat(y+1, x-1, +1, -1),
+                GetFirstSeat(y+1, x, +1, 0),
+                GetFirstSeat(y+1, x+1, +1, +1),
+            };
+
+            return tilesAround.Count(z => z == OccupiedSeat);
+        }
+
+        private char GetFirstSeat(int y, int x, int yChange, int xChange)
+        {
+            if (y < 0 || y > WaitingRoomMaxY || x < 0 || x > WaitingRoomMaxX)
+            {
+                return EmptySeat;
+            }
+
+            var tile = WaitingArea[y][x];
+            return tile == Floor ? 
+                GetFirstSeat(y - yChange, x - xChange, yChange, xChange) : 
+                tile;
         }
     }
 }
